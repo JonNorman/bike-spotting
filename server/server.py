9ad06f5e40d87ec066fa36d5e5040d7c536ca0fa
Karl Barker <karlbarker1@gmail.com>
2015-09-26 16:37:24 +0100

    Add index html

--- server/server.py
+++ server/server.py
@@ -1,9 +1,10 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
-from pyramid.response import Response
+from pyramid.response import Response, FileResponse
 import requests as rq
 import xml.etree.ElementTree as et
 import json
+import os
 
 class Station(object):
 
@@ -31,10 +32,15 @@ def home(request):
     stations = parse_xml(tree)
     return Response("{:s}".format(json.dumps(stations, default=Station.serialize)))
 
+def index(request):
+    return FileResponse(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'views/index.html'))
+
 if __name__ == "__main__":
     config = Configurator()
     config.add_route("home", "/")
     config.add_view(home, route_name="home")
+    config.add_route("index", "/")
+    config.add_view(index, route_name="index")
     app = config.make_wsgi_app()
     server = make_server("0.0.0.0", 8080, app)
     server.serve_forever()
