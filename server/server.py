ceb6bac6ce0e78f1d3197b44c17ba1db1220b298
Jon Norman <jon.alex.norman@gmail.com>
2015-09-26 16:29:52 +0100

    refactoring back-end

--- server/server.py
+++ server/server.py
@@ -1,40 +1,37 @@
+"""
+    This module runs the back-end server to the bike spotting website
+"""
+# pyramid modules
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+
+# http modules and station/serialization
 import requests as rq
-import xml.etree.ElementTree as et
-import json
+import station
 
-class Station(object):
+def bikes(request):
+    """
+    defines the response when directing to the "bikes" route.
+    """
+    # pick up the latest bike statuses
+    api_uri = "https://tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml"
+    r = rq.get(api_uri)
+    
+    # build a dictionary of stations from the API response
+    stations = station.StationCollection()
+    stations.from_xml_string(r.text)
 
-    def __init__(self, attributes):
-        self.props = attributes
-
-    def serialize(self):
-        return self.props
-
-    def __str__(self):
-        return "ID: {id} Name: {name}".format(**self.props)
-
-def parse_xml(tree):
-    stations = {}
-    for elem in tree.getchildren():
-        props = elem.getchildren()
-        station = Station({prop.tag: prop.text for prop in props})
-        stations[station.props['id']] = station
-
-    return stations
-
-def home(request):
-    r = rq.get("https://tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml")
-    tree = et.fromstring(r.text)
-    stations = parse_xml(tree)
-    return Response("{:s}".format(json.dumps(stations, default=Station.serialize)))
+    return Response(stations.to_json())
 
 if __name__ == "__main__":
     config = Configurator()
-    config.add_route("home", "/")
-    config.add_view(home, route_name="home")
+    
+    # add the routes and views
+    config.add_route("bikes", "/bikes")
+    config.add_view(bikes, route_name="bikes")
     app = config.make_wsgi_app()
+
+    # start the server
     server = make_server("0.0.0.0", 8080, app)
     server.serve_forever()
